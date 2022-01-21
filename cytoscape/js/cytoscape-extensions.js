ee5e0d470231cff81dbec3d668d8af49516370a3
smerdyakov <73742467+oyuz@users.noreply.github.com>
2022-01-21 11:02:28 +0100

    Default zoom level fixed

--- cytoscape/js/cytoscape-extensions.js
+++ cytoscape/js/cytoscape-extensions.js
@@ -114,7 +114,7 @@ var defaults = {
   // Whether to fit the network view after when done
   fit: true,
   // Padding on fit
-  padding: 10,
+  padding: 150,
   // Whether to enable incremental mode
   randomize: true,
   // Node repulsion (non overlapping) multiplier
